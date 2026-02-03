03f49e2b3027b17469c1ef4d82a3546285987817
jcampbell7472 <jcampbell7472@gmail.com>
2026-02-03 10:54:21 +0000

    Initial Spring Boot project setup

---
+++ backend/ggst_stats/src/main/java/io/github/jcampbell7472/ggst_stats/GgstStatsApplication.java
@@ -0,0 +1,13 @@
+package io.github.jcampbell7472.ggst_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgstStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgstStatsApplication.class, args);
+	}
+
+}
---
+++ backend/ggst_stats/src/test/java/io/github/jcampbell7472/ggst_stats/GgstStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jcampbell7472.ggst_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgstStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
